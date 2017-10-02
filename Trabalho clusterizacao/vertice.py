cb864e0512c8ed1dccad99ff8a1764cbfb7bd0e2
Igor Lima <igorlima_es@alu.ufc.br>
2017-10-02 19:47:09 -0300

    adicionando construtor para a classe Vertice com os atributos taxitaxi_id, date_time, longitude, latitude e os getters dos mesmos

--- Trabalho clusterizacao/vertice.py
+++ Trabalho clusterizacao/vertice.py
@@ -1,11 +1,17 @@
 class Vertice:
-    def __init__(self, tax):
-        self.teste = teste
-    def soma(self):
-        return self.teste
+    def __init__(self, taxi_id, date_time, longitude, latitude):
+        self.taxi_id = taxi_id
+        self.date_time = date_time
+        self.longitude = longitude
+        self.latitude = latitude
+    def get_taxi_id(self):
+        return self.taxi_id
 
+    def get_date_time(self):
+        return self.date_time
 
+    def get_longitude(self):
+        return self.longitude
 
-vertice = Vertice("oi")
-a = vertice.soma()
-print(a)
+    def get_latitude(self):
+        return self.latitude
