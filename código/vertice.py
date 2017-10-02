7ea9f969ac67adcea8dbb09690df4c4913763213
Igor Lima <igorlima_es@alu.ufc.br>
2017-10-02 19:32:37 -0300

    Adicionando começo do primeiro trabalho

---
+++ código/vertice.py
@@ -0,0 +1,11 @@
+class Vertice:
+    def __init__(self, tax):
+        self.teste = teste
+    def soma(self):
+        return self.teste
+
+
+
+vertice = Vertice("oi")
+a = vertice.soma()
+print(a)
