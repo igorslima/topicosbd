c621f4ade992e4f7a477d1c5818eae1a52e7144f
Igor Lima <igorlima_es@alu.ufc.br>
2017-10-04 11:07:33 -0300

    Adicionado base de dados, adicionado funcao que transforma dados em pd.DataFrame()

--- Trabalho clusterizacao/DAO/verticeDAO.py
+++ Trabalho clusterizacao/DAO/verticeDAO.py
@@ -1,7 +1,14 @@
 import sys
 sys.path.append("..") # "Sobe" uma pasta para que seja possível importar o model.vertice.Vertice
-from model.vertice import *
 
+#para saber o caminho do arquivo
+#import os 
+#dir_path = os.path.dirname(os.path.realpath(__file__))
+#print(dir_path)
+
+from model.vertice import *
+import pandas as pd
+import os
 class vertice_Dao:
     def __init__(self):
         pass
@@ -12,7 +19,20 @@ class vertice_Dao:
             vertice = Vertice(linha[0], linha[1], linha[2], linha[3])
             lista.append(vertice)
         return lista
-
+    def join_all_files(self):
+        df = pd.DataFrame()
+        df['id_taxista'] = 0
+        df['date_time'] = 0
+        df['longitude'] = 0
+        df['latitude'] = 0
+        lista_arquivos = os.listdir("../taxi_log_2008_by_id/")
+        cont = 0
+        for arquivo in lista_arquivos:
+            lista = self.file_to_vert_list('../taxi_log_2008_by_id/{}'.format(arquivo))
+            print(cont)
+            cont += 1
+        return df
+        #return df
 dao = vertice_Dao()
-lista_ = dao.file_to_vert_list("../1.txt")
-print(lista_[0].get_latitude())
+#lista_ = dao.file_to_vert_list("../1.txt")
+dao.join_all_files()
