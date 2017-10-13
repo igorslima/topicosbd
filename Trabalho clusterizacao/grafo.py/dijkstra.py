c77336a3c13b7b42d970b4666e0541ef6045cc4b
Igor Lima <igor_ce@outlook.com>
2017-10-13 15:24:06 -0300

    mexendo no dijkstra

--- Trabalho clusterizacao/grafo.py/dijkstra.py
+++ Trabalho clusterizacao/grafo.py/dijkstra.py
@@ -1,8 +1,23 @@
-# TODO IMPLEMENTAR DJKSTRA
+import sys
+class Dijkstra:
+    def __init__(self, conjunto_vertices, conjunto_aresta):
+        self.vertices = conjunto_vertices
+        self.arestas = conjunto_aresta
+        self.distancias = []
 
-class Grafo:
-    def __init__(self, vertices):
-        self.qtdVertices = vertices
-        self.vizinhos = {}
-        for vertice in vertice:
-            vizinhos.append(vertice)
+    def dijkstra(self, inicio, fim):
+        self.distancias = []
+        for vertice in self.vertices:
+            self.distancias.append([vertice.id, sys.float_info.max])
+    
+    def find_vertice(self, id_vertice):
+        for vertice in self.vertices:
+            if vertice.id == id_vertice:
+                return vertice
+        return None
+    def find_vertices(self, id_vertice):
+        lista = []
+        for aresta in self.arestas:
+            if(aresta.inicio == id_vertice):
+                lista.append(aresta)
+        return lista
--- Trabalho clusterizacao/model/vertice.py
+++ Trabalho clusterizacao/model/vertice.py
@@ -1,6 +1,6 @@
 class Vertice:
-    def __init__(self,id,ponto):
-        self.id = id
+    def __init__(self,id_vertice,ponto):
+        self.id = id_vertice
         self.ponto = ponto
         self.proximoVertice = None
 
